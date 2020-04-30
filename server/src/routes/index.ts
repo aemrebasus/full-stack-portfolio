44f46a4d367977358a121bbdaf87f24da0d3d794
Ahmet Emrebas <aemrebasus@gmail.com>
2020-04-29 19:11:23 -0500

    Created User's router and tested GET, POST, UDPATE DELETE closes #26 closes #28

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { information } from './information';
 import { issue } from './issue';
+import { user } from './user';
 
 export const routers = [
     information,
-    issue
+    issue,
+    user
 ]
 
